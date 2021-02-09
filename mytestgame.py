6458999ac3a45d833af70a94edfad6d4f51273c4
coronadoapps <36603499+coronadoapps@users.noreply.github.com>
2021-02-09 18:17:16 -0500

    Mouse tracking
    Posicion y seguimiento del mouse dentro del screen

--- mytestgame.py
+++ mytestgame.py
@@ -2,14 +2,14 @@ import pygame, sys, random
 from pygame.locals import *
 
 #COLORS
-BACKGROUND = (34,34,34)
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-GRASS = (11, 102, 35)
-SKY = (135, 206, 235)
+BACKGROUND =    (34,34,34)
+WHITE      =    (255,255,255)
+BLACK      =    (0,0,0)
+RED        =    (255,0,0)
+GREEN      =    (0,255,0)
+BLUE       =    (0,0,255)
+GRASS      =    (11, 102, 35)
+SKY        =    (135, 206, 235)
 
 #Initialize the pygame
 pygame.init()
@@ -28,39 +28,32 @@ height = 50
 x = WINDOW_WIDTH/2
 y = WINDOW_HEIGHT/2
 
-dx = 7
-dy = 7
+dx = 10
+dy = 10
 
 isJump = False
 jumpCount = 10
 
-def close():
-    pygame.quit()
-    sys.exit()
-    
 
 def drawGrid():
     blockSize = 80 #Set the size of the grid block
     for x in range(WINDOW_WIDTH):
         for y in range(WINDOW_HEIGHT):
-            rect = pygame.Rect(x*blockSize, y*blockSize,
-                               blockSize, blockSize)
+            rect = pygame.Rect(x*blockSize, y*blockSize, blockSize, blockSize)
             pygame.draw.rect(screen, WHITE, rect, 1)
 
-def drawFood():
-    random_color = (random.uniform(0,255), random.uniform(0,255), random.uniform(0,255))
-    random_pos = (random.uniform(0,WINDOW_WIDTH), random.uniform(0,WINDOW_HEIGHT))
-
-    for x in range(WINDOW_WIDTH):
-        for y in range(WINDOW_HEIGHT):
-            pygame.draw.circle(screen, random_color, random_pos, 10)
-
+def close():
+    pygame.quit()
+    sys.exit()
 
 while True:
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT :
+        if event.type == pygame.QUIT:
             close()
+        elif event.type == KEYDOWN:
+               if event.key == K_ESCAPE:
+                   close()
 
     keys = pygame.key.get_pressed()
     
@@ -77,8 +70,8 @@ while True:
         if keys[pygame.K_DOWN] and y < 640 - height - dy:
             y += dy
 
-        #if keys[pygame.K_SPACE]:
-            #isJump = True
+        if keys[pygame.K_SPACE]:
+            isJump = True
     else:
         if jumpCount >= -10:
             y -= (jumpCount * abs(jumpCount)) * 0.5
@@ -89,9 +82,11 @@ while True:
 
     screen.fill(BACKGROUND)
     #-------------------
-    pygame.draw.rect(screen, WHITE, (x, y, width, height))
+    (x,y) = pygame.mouse.get_pos()
+    #pygame.draw.rect(screen, WHITE, (x, y, width, height))
+    pygame.draw.circle(screen, RED, (x, y), 15)
+    #drawGrid()
+    print(x,y)
     #-------------------
     pygame.display.update()
     clock.tick(60)
-
-
